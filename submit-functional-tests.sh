64c42ca4d2da0665473172341a1dc4dbdfb4c60e
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2023-10-15 15:11:18 +0900

    Add a script to submit all jobs
    Signed-off-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>

---
+++ submit-functional-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get environment variables
+TOKEN=$(cat ./env/jenkins.token)
+HOST=$(cat ./env/jenkins.host)
+
+# Run all functional tests
+for job in $(ls **/functional-tests/*.yaml);
+do
+	echo Submitting $job ...
+	cp $job _submit.yaml
+	
+	lavacli \
+	--uri http://hyeyoo:$TOKEN@$HOST/RPC2/ jobs submit _submit.yaml
+	rm _submit.yaml
+done
