22a47c0bc0f83c2a76e74c8c0194fa946a6a9b63
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2023-10-18 21:53:18 +0900

    set JENKINS_{PROJECT_NAME,BUILD_ID} env for gcov submission
    Signed-off-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>

--- submit-functional-tests.sh
+++ submit-functional-tests.sh
@@ -14,6 +14,8 @@ for job in $(ls ./lava-jobs-functional/*.yaml);
 do
 	echo Submitting $job...
 	sed "s/KERNEL/${KERNEL//\//\\/}/g" $job > _submit.yaml
+	sed -i "s/JENKINS_BUILD_ID_VALUE/\"$JENKINS_BUILD_ID\"/g" _submit.yaml
+	sed -i "s/JENKINS_PROJECT_NAME_VALUE/\"$JENKINS_PROJECT_NAME\"/g" _submit.yaml
 	lavacli \
 	--uri https://hyeyoo:$TOKEN@$HOST/RPC2/ jobs submit _submit.yaml
 	rm _submit.yaml
