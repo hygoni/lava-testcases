750347e08fa82becfc9483bc45100954b79eac08
Harry Yoo <harry.yoo@oracle.com>
2025-05-19 23:50:31 +0900

    Directly request to LAVA
    Signed-off-by: Harry Yoo <harry.yoo@oracle.com>

--- submit-functional-tests.sh
+++ submit-functional-tests.sh
@@ -10,14 +10,9 @@ for job in $(ls ./lava-jobs-functional/*.yaml);
 do
 	echo Submitting $job...
 	sed "s/KERNEL/${KERNEL//\//\\/}/g" $job > _submit.yaml
-	sed -i "s/TREE_NAME_VALUE/\"$TREE_NAME\"/g" _submit.yaml
 
-	hash=$(sha1sum _submit.yaml | cut -c 1-6)
-	scp ./_submit.yaml hyeyoo@$LAVA_IP:/tmp/job-$hash.yaml
-	ssh hyeyoo@$LAVA_IP "lavacli \
-	--uri http://hyeyoo:$LAVA_TOKEN@localhost/RPC2/ jobs submit /tmp/job-$hash.yaml"
-	ssh hyeyoo@$LAVA_IP "rm /tmp/job-$hash.yaml"
-
+	lavacli \
+	--uri http://hyeyoo:$LAVA_TOKEN@lava.kerneltesting.org/RPC2/ jobs submit _submit.yaml
 	rm _submit.yaml
 done
 
