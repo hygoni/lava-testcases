e9ff47a66a263a1d74a90c75c15935aa890984b1
Harry Yoo <harry.yoo@oracle.com>
2025-05-20 14:09:16 +0900

    Fix BUILD_ID

--- submit-functional-tests.sh
+++ submit-functional-tests.sh
@@ -10,7 +10,7 @@ for job in $(ls ./lava-jobs-functional/*.yaml);
 do
 	echo Submitting $job...
 	sed "s/KERNEL/${KERNEL//\//\\/}/g" $job > _submit.yaml
-	sed -i "s/BUILD_ID_VALUE/${BUILD_ID//\//\\/}/g" $job
+	sed -i "s/BUILD_ID_VALUE/${BUILD_ID//\//\\/}/g" _submit.yaml
 
 	lavacli \
 	--uri http://hyeyoo:$LAVA_TOKEN@lava.kerneltesting.org/RPC2/ jobs submit _submit.yaml
